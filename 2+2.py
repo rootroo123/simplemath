603bf5a0138fb3f54bac01420c31b9c154e3ab28
rootroo123 <74721856+rootroo123@users.noreply.github.com>
2020-11-19 09:31:27 -0500

    Add files via upload

---
+++ 2+2.py
@@ -0,0 +1,10 @@
+print ('welcome to trivia')
+print ('this is a one question simple math equation')
+score = 0
+total_q = 1
+ans=input ('what is 2+2?')
+if ans == '4':
+    score+=1
+    print ('correct')
+else: 
+    print ("incorrect")
